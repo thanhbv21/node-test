5b5a228a53cfd9bd6f263b401b7ac08119c1ea4e
thanh.bui <thanh.bui@niteco.se>
2019-11-04 11:12:47 +0700

    update barcode check

--- routes/qrCode.js
+++ routes/qrCode.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(`response ${req.params.code}`);
+const data = [{
+    code: '123123',
+    data: 'https://maqr.vn/XT72603377147'
+}]
+
+router.get('/:code', function(req, res, next) {
+  let response;
+  data.forEach(item => {
+    if(item.code === req.params.code) 
+        response = {
+            "code": 200,
+            "data": item.data
+        };
+  });
+
+  response = (!response) ? { "code" : 400, "desc" : { "st1" : "SẢN PHẨM CHƯA ĐƯỢC BÁN RA THỊ TRƯỜNG", "st2" : "Xin quý khách cẩn thận!" } } : response;
+  
+  res.json(response);
 });
 
 module.exports = router;
